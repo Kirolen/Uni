29db079bf6e030eec95c51978b19b2bbf52fac78
Kostiantyn Mitskevich <70913524+Kirolen@users.noreply.github.com>
2024-02-08 20:31:26 +0200

    Update Unrolled_Link_List.cpp
    - Added prev pointer for struct Node
    - Added find, remove, set and get functions

--- Cour 2/labs/lab #2/Unrolled_Link_List.cpp
+++ Cour 2/labs/lab #2/Unrolled_Link_List.cpp
@@ -1,8 +1,10 @@
-
 #include <iostream>
 #include <cmath>
+
 using namespace std;
+
 #define maxElements 4  
+#define zeroIndex 0
 
 template <typename T>
 class UnLinList
@@ -15,20 +17,24 @@ private:
         int numElements;
         T data[maxElements];
         Node* next;
+        Node* prev;
 
-        Node(int numElements = 0, Node* next = nullptr)
+        Node(int numElements = 0, Node* next = nullptr, Node* prev = nullptr)
         {
             this->numElements = numElements;
             this->next = next;
+            this->prev = prev;
         }
     };
 
+    Node* foot;
     Node* head;
     int size;
 
 public:
     UnLinList()
     {
+        foot = nullptr;
         head = nullptr;
         size = 0;
     }
@@ -39,35 +45,39 @@ public:
         {
             head = new Node();
             head->data[head->numElements++] = value;
+            foot = head;
         }
         else 
         {
-            Node* current = head;
-            while (current->next) { current = current->next; }
-
-            if (current->numElements < ceil(maxElements/2)) { current->data[current->numElements++] = value; }
+            if (foot->numElements < ceil(maxElements/2)) 
+            {
+                foot->data[foot->numElements++] = value;
+            }
             else
             {
-                current->next = new Node;
-                current->next->data[current->next->numElements++] = value;
+                foot->next = new Node;
+                foot->next->data[foot->next->numElements++] = value;
+                foot->next->prev = foot;
+                foot = foot->next;
             }
         }
         size++;
     }
     void insert(int index, const T& value)
     {
+        const int startIndex = index;
         if (index < 0 || index > size)
         {
-            cout << "\n### Error: incorrect index! ###\n";
+            cerr << "\n### Error: incorrect index! ###\n";
             return;
         }
 
-        if (index == 0) insertBegin(value);
+        if (index == 0 && !head) insertBegin(value);
         else
         {
             int currentIndex = 0;
             Node* current = head;
-            while (current && currentIndex + current->numElements <= index)
+            while (current && currentIndex + current->numElements < index)
             {
                 currentIndex += current->numElements;
                 current = current->next;
@@ -75,14 +85,16 @@ public:
 
             if (!current)
             {
-                cout << "\n### Error: unable to find the node before the given index! ###\n";
+                cerr << "\n### Error: unable to find the node before the given index! ###\n";
                 return;
             }
 
             index -= currentIndex;
             if (current->numElements < maxElements)
             {
-                for (int i = current->numElements; i > index; i--)   current->data[i] = current->data[i - 1];
+                for (int i = current->numElements; i > index; i--) 
+                    current->data[i] = current->data[i - 1];
+
                 current->data[index] = value;
                 current->numElements++;
                 size++;
@@ -90,25 +102,36 @@ public:
             else if (current->next && current->next->numElements < maxElements)
             {
                 Node* nextNode = current->next;
-                for (int i = nextNode->numElements; i >= 0; i--)     nextNode->data[i] = nextNode->data[i-1];
-                nextNode->data[0] = current->data[maxElements - 1];
+                for (int i = nextNode->numElements; i > 0; i--)  
+                    nextNode->data[i] = nextNode->data[i-1];
+
+                nextNode->data[zeroIndex] = current->data[maxElements - 1];
                 nextNode->numElements++;
-                current->data[maxElements - 1] = value;
-                size++;
+                current->numElements--;
+
+                insert(startIndex, value);
             }
             else
             {
                 Node* newNode = new Node();
                 int mid = maxElements / 2;
-                for (int i = mid; i < maxElements; ++i)
-                    newNode->data[i - mid] = current->data[i];
-                newNode->numElements = maxElements - mid;
-                current->numElements = mid;
 
+                for (int i = 0; i < mid; i++)
+                {
+                    newNode->data[i] = current->data[mid + i];
+                    newNode->numElements++;
+                    current->numElements--;
+                }
+                
+                newNode->prev = current;
                 newNode->next = current->next;
+                if (current->next)
+                    current->next->prev = newNode;
+                else
+                    foot = newNode;
                 current->next = newNode;
 
-                insert(index, value);
+                insert(startIndex, value);
             }
         }
     }
@@ -116,33 +139,127 @@ public:
     {
         if (!head || head->numElements < maxElements)
         {
-            if (!head) head = new Node();
-            for (int i = head->numElements; i > 0; i--) head->data[i] = head->data[i - 1];
-            head->data[0] = value;
+            if (!head) 
+            {
+                head = new Node();
+                foot = head;
+            }
+
+            for (int i = head->numElements; i > 0; i--) 
+                head->data[i] = head->data[i - 1];
+
+            head->data[zeroIndex] = value;
             head->numElements++;
             size++;
         }
-        else if (head->next&& head->next->numElements < maxElements)
-        {
-            Node* nextNode = head->next;
-            for (int i = nextNode->numElements; i > 0; i--)   nextNode->data[i] = nextNode->data[i - 1];
-            nextNode->data[0] = head->data[maxElements - 1];
-            nextNode->numElements++;
-            head->data[maxElements - 1] = value;
-            size++;
-        }
         else
-        {
-            Node* newHead = new Node();
-            int mid = head->numElements / 2;
-            for (int i = mid; i < head->numElements; ++i)   newHead->data[i - mid] = head->data[i];
-            newHead->numElements = head->numElements - mid;
-            head->numElements = mid;
-            newHead->next = head->next;
-            head->next = newHead;
+            insert(zeroIndex, value);
+    }
 
-            insertBegin(value);
+    void remove(int index)
+    {
+        if (index < 0 || index > size)
+        {
+            cerr << "\n### Error: incorrect index! ###\n";
+            return;
         }
+
+        int currentIndex = 0;
+        Node* current = head;
+        while (current && currentIndex + current->numElements < index)
+        {
+            currentIndex += current->numElements;
+            current = current->next;
+        }
+
+        if (!current)
+        {
+            cerr << "\n### Error: unable to find the node before the given index! ###\n";
+            return;
+        }
+
+        index -= currentIndex - 1;
+        for (int i = index; i < current->numElements - 1; i++)
+        {
+            current->data[i] = current->data[i + 1];
+        }
+        current->numElements--;
+
+        if (current->numElements == 0)
+        {
+            if (current == head)
+            {
+                head = current->next;
+                if (head)
+                    head->prev = nullptr;
+            }
+            else if (current == foot)
+            {
+                Node* prev = foot->prev;
+                foot = prev;
+                prev->next = nullptr;
+            }
+            else
+            {
+                current->prev->next = current->next;
+                if (current->next)
+                    current->next->prev = current->prev;
+            }
+
+            delete current;
+        }
+        size--;
+    }
+
+    T get(int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw ("\n### Error: incorrect index! ###\n");
+        }
+
+        int currentIndex = 0;
+        Node* current = head;
+        while (current && currentIndex + current->numElements <= index)
+        {
+            currentIndex += current->numElements;
+            current = current->next;
+        }
+    
+        return current->data[index -= currentIndex];
+    }
+    void set(int index, T value)
+    {
+        if (index < 0 || index > size)
+        {
+            cerr << "\n### Error: incorrect index! ###\n";
+        }
+
+        int currentIndex = 0;
+        Node* current = head;
+        while (current && currentIndex + current->numElements < index)
+        {
+            currentIndex += current->numElements;
+            current = current->next;
+        }
+
+        current->data[currentIndex] = value;
+    }
+
+    int find(T value)
+    {
+        Node* current = head;
+        int index = 0;
+        while (current)
+        {
+            for(int i = 0; i < current->numElements; i++)
+            {
+                if (current->data[i] == value) return index;
+                index++;
+            }
+            current = current->next;
+        }
+        throw ("\n### Element didn`t found ###\n");
     }
 
     void print()
@@ -154,10 +271,28 @@ public:
             cout << "[ ";
             for (int i = 0; i < current->numElements; i++)
             {
-                cout << current->data[i] << ", ";
+                cout << current->data[i];
+                if (i != current->numElements - 1) cout << ", ";
             }
-            cout << "] --> ";
+            cout << " ] --> ";
             current = current->next;
+        }
+        cout << "nullptr\n";
+    }
+    void reverse_print()
+    {
+        Node* current = foot;
+
+        while (current)
+        {
+            cout << "[ ";
+            for (int i = current->numElements - 1; i >= 0 ; i--)
+            {
+                cout << current->data[i];
+                if (i != 0) cout << ", ";
+            }
+            cout << " ] --> ";
+            current = current->prev;
         }
         cout << "nullptr\n";
     }
@@ -184,23 +319,62 @@ int main()
 void demonstrationMode()
 {
     UnLinList<int> myUnrolled;
-
-    myUnrolled.push_back(1);
-    myUnrolled.push_back(2);
-    myUnrolled.push_back(3);
-    myUnrolled.push_back(4);
-
-    //myUnrolled.print();
-
-    myUnrolled.push_back(5);
-   // myUnrolled.print();
-
-    //cout << "ADd 6 on 0:\n";
     myUnrolled.insert(0, 6);
     myUnrolled.insert(0, 6);
+    myUnrolled.insertBegin(6);
+    myUnrolled.insert(0, 6);
+    myUnrolled.insert(0, 6);
+    myUnrolled.insertBegin(6);
     myUnrolled.print();
+    cout << "================================\n";
 
-
-    myUnrolled.insert(6, 14);
+    myUnrolled.insert(3, 1);
     myUnrolled.print();
+    cout << "================================\n";
+
+    myUnrolled.insert(3, 2);
+    myUnrolled.print();
+    cout << "================================\n";
+
+    myUnrolled.insert(3, 3);
+    myUnrolled.print();
+    cout << "================================\n";
+
+    myUnrolled.insert(3, 4);
+    myUnrolled.print();
+    cout << "================================\n";
+
+    myUnrolled.insert(3, 5);
+    myUnrolled.print();
+    cout << "================================\n";
+    myUnrolled.insert(11, 100);
+    myUnrolled.print();
+    cout << "================================\n";
+
+    myUnrolled.remove(5);
+    myUnrolled.remove(11);
+    myUnrolled.remove(10);
+    myUnrolled.print();
+    cout << "================================\n";
+
+    try { cout << "Get 0: " << myUnrolled.get(0) << endl; }
+    catch (const char* errorMessage) { cerr << "Error: " << errorMessage << std::endl; }
+    try { cout << "Get 5: " << myUnrolled.get(5) << endl; }
+    catch (const char* errorMessage) { cerr << "Error: " << errorMessage << std::endl; }
+    try { cout << "Get 6: " << myUnrolled.get(6) << endl; }
+    catch (const char* errorMessage) { cerr << "Error: " << errorMessage << std::endl; }
+
+    myUnrolled.set(0, 99);
+    myUnrolled.print();
+    cout << "================================\n";
+        
+    try { cout << "Find 99: " << myUnrolled.find(99) << endl; }
+    catch (const char* errorMessage) { cerr << "Error: " << errorMessage << std::endl; };
+    try { cout << "Find 1000: " << myUnrolled.find(1000) << endl; }
+    catch (const char* errorMessage) { cerr << "Error: " << errorMessage << std::endl; }
+    try { cout << "Find 3: " << myUnrolled.find(3) << endl; }
+    catch (const char* errorMessage) { cerr << "Error: " << errorMessage << std::endl; };
+
+    cout << "\n\n\n\n";
+    myUnrolled.reverse_print();
 }
